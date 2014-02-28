30dcb76f3d482ed517028c291e3f77d1c7c534a5
Grant Marshall <grant.a.marshall@asu.edu>
2014-02-27 23:42:58 -0700

    Change the type of the mapping of the Group type

--- src/GrCal/Types.hs
+++ src/GrCal/Types.hs
@@ -4,5 +4,5 @@ import qualified Data.Map as M
 import qualified Data.Set as S
 
 newtype Element = Element String
-data Group = Group (S.Set Element) (M.Map Element Element)
+data Group = Group (S.Set Element) (M.Map (Element, Element) Element)
 
