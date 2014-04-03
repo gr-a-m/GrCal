cfd5d53b393397c1d37246eae66bcb359d5ebf9a
Grant Marshall <grant.a.marshall@asu.edu>
2014-04-03 16:47:48 -0700

    Added results of zstar experiment -- zstar.nb contains factorizations of cyclic zStar groups up to n = 1000

--- src/GrCal/Experiments.hs
+++ src/GrCal/Experiments.hs
@@ -1,6 +1,11 @@
 module GrCal.Experiments where
 
+import Data.List(foldl')
+import qualified Data.Map as M
+
 import GrCal.Parsing
+import GrCal.Group (cyclic)
+import GrCal.GroupInput (checkAll, convertTable, InputErrors)
 
 -- |This function takes the parameters for the indirect product group and
 -- the list of pre-computed elements and makes a 2d list of the group
@@ -31,4 +36,34 @@ indirectProductTable n m a =
 -- two points and computes the indirect product with this information.
 indirectProduct :: Int -> Int -> Int -> (Int, Int) -> (Int, Int) -> (Int, Int)
 indirectProduct n m a (u, v) (w, y) = ((u + w * a^y) `mod` n, (v + y) `mod` m)
+
+validMap :: Int -> Int -> M.Map (Int, Int, Int) Bool
+validMap maxN maxM =
+  let
+    params = [(n, m, a) | n <- [1 .. maxN], m <- [1 .. maxM], a <- [2 .. (n - 1)], gcd n a == 1]
+  in
+    foldl' insertEither M.empty params
+
+insertEither :: M.Map (Int, Int, Int) Bool -> (Int, Int, Int) -> M.Map (Int, Int, Int) Bool
+insertEither ma (n, m, a) =
+  case checkAll $ indirectProductTable n m a of
+    Left ie -> M.insert (n, m, a) False ma
+    Right _ -> ma
+
+checkZStar :: Int -> [Int]
+checkZStar n =
+  filter (cyclic . convertTable . zStarTable) [2 .. n]
+
+zStarOp :: Int -> [Int] -> [[Int]]
+zStarOp n elems =
+  [[(x * y) `mod` n | y <- elems] | x <- elems]
+
+zStarTable :: Int -> Table
+zStarTable n =
+  let
+    elements = [a | a <- [1 .. (n - 1)], gcd a n == 1]
+    elementRow = TableLine $ map (makeElem . show) elements
+    operationRows = map (TableLine . map (makeElem . show)) $ zStarOp n elements
+  in
+    Table elementRow operationRows
 
