bd8a6668c5ddda6906b1443ee0b77371436a2a76
Grant Marshall <gam@grantamarshall.com>
2014-04-17 14:31:06 -0700

    Fix semidirect product to have correct name; Fix semidirectProduct function to compute correctly (typo)

--- src/GrCal/Experiments.hs
+++ src/GrCal/Experiments.hs
@@ -13,19 +13,19 @@ import GrCal.GroupInput (checkAll, convertTable, InputErrors)
 ipMap :: Int -> Int -> Int -> [(Int, Int)] -> [[(Int, Int)]]
 ipMap n m a elems =
   let
-    groupOp = indirectProduct n m a
+    groupOp = semidirectProduct n m a
   in
     [[groupOp x y | y <- elems] | x <- elems]
 
 -- |Produce a table for the possible group of Z_n x Z_m with the indirect
 -- product as the group law.
-indirectProductTable :: Int -> Int -> Int -> Table
-indirectProductTable n m a =
+semidirectProductTable :: Int -> Int -> Int -> Table
+semidirectProductTable n m a =
   if a > n then
-    indirectProductTable n m (a `mod` n)
+    semidirectProductTable n m (a `mod` n)
   else
     let
-      groupOp = indirectProduct n m a
+      groupOp = semidirectProduct n m a
       elements = [(u, v) | u <- [0 .. (n - 1)], v <- [0 .. (m - 1)]]
       elementRow = TableLine $ map (makeElem . show) elements
       operationRows = map (TableLine . map (makeElem . show)) $ ipMap n m a elements
@@ -34,8 +34,8 @@ indirectProductTable n m a =
 
 -- |This function takes the groups of the coordinates, the value of a, and
 -- two points and computes the indirect product with this information.
-indirectProduct :: Int -> Int -> Int -> (Int, Int) -> (Int, Int) -> (Int, Int)
-indirectProduct n m a (u, v) (w, y) = ((u + w * a^y) `mod` n, (v + y) `mod` m)
+semidirectProduct :: Int -> Int -> Int -> (Int, Int) -> (Int, Int) -> (Int, Int)
+semidirectProduct n m a (u, v) (w, y) = ((u + (w * (a^v))) `rem` n, (v + y) `rem` m)
 
 invalidMap :: Int -> Int -> M.Map (Int, Int, Int) InputErrors
 invalidMap maxN maxM =
@@ -46,7 +46,7 @@ invalidMap maxN maxM =
 
 insertEither :: M.Map (Int, Int, Int) InputErrors -> (Int, Int, Int) -> M.Map (Int, Int, Int) InputErrors
 insertEither ma (n, m, a) =
-  case checkAll $ indirectProductTable n m a of
+  case checkAll $ semidirectProductTable n m a of
     Left ie -> M.insert (n, m, a) ie ma
     Right _ -> ma
 
@@ -66,7 +66,7 @@ validInvalid' n maxM =
 
 accumPoints :: ([(Int, Int, Int)], [(Int, Int, Int)]) -> (Int, Int, Int) -> ([(Int, Int, Int)], [(Int, Int, Int)])
 accumPoints (xs, ys) (n, m, a) =
-  case checkAll $ indirectProductTable n m a of
+  case checkAll $ semidirectProductTable n m a of
     Left _ -> ((n, m, a) : xs, ys)
     Right _ -> (xs, (n, m, a) : ys)
 
